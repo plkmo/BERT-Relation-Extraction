48436363de67ec2621024e3a4a6e3218334f9267
JensMadsen <plasmajens@gmail.com>
2020-08-26 15:58:51 +0200

    add spacy model used for pretraining corpus bulding

--- main_pretraining.py
+++ main_pretraining.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-"""
+'''
 Created on Wed Nov 27 11:16:26 2019
 
 @author: weetee
-"""
+'''
 
 from src.trainer import train_and_fit
 import logging
@@ -18,26 +18,27 @@ logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
 logger = logging.getLogger('__file__')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--pretrain_data", type=str, default="./data/cnn.txt",
-                        help="pre-training data .txt file path")
-    parser.add_argument("--batch_size", type=int, default=32, help="Training batch size")
-    parser.add_argument("--freeze", type=int, default=0, help='''1: Freeze most layers until classifier layers\
+    parser.add_argument('--pretrain_data', type=str, default='./data/cnn.txt',
+                        help='pre-training data .txt file path')
+    parser.add_argument('--batch_size', type=int, default=32, help='Training batch size')
+    parser.add_argument('--freeze', type=int, default=0, help='''1: Freeze most layers until classifier layers\
                                                                 \n0: Don\'t freeze \
                                                                 (Probably best not to freeze if GPU memory is sufficient)''')
-    parser.add_argument("--gradient_acc_steps", type=int, default=2, help="No. of steps of gradient accumulation")
-    parser.add_argument("--max_norm", type=float, default=1.0, help="Clipped gradient norm")
-    parser.add_argument("--fp16", type=int, default=0, help="1: use mixed precision ; 0: use floating point 32")  # mixed precision doesn't seem to train well
-    parser.add_argument("--num_epochs", type=int, default=18, help="No of epochs")
-    parser.add_argument("--lr", type=float, default=0.0001, help="learning rate")
-    parser.add_argument("--model_type", type=str, default='BERT', choices=('ALBERT', 'BERT', 'BioBERT'),
+    parser.add_argument('--gradient_acc_steps', type=int, default=2, help='No. of steps of gradient accumulation')
+    parser.add_argument('--max_norm', type=float, default=1.0, help='Clipped gradient norm')
+    parser.add_argument('--fp16', type=int, default=0, help='1: use mixed precision ; 0: use floating point 32')  # mixed precision doesn't seem to train well
+    parser.add_argument('--num_epochs', type=int, default=18, help='No of epochs')
+    parser.add_argument('--lr', type=float, default=0.0001, help='learning rate')
+    parser.add_argument('--model_type', type=str, default='BERT', choices=('ALBERT', 'BERT', 'BioBERT'),
                         help='''Model  BERT\nALBERT\nBioBERT''')
-    parser.add_argument("--model_size", type=str, default='bert-base-uncased', help="For BERT: 'bert-base-uncased', \
+    parser.add_argument('--model_size', type=str, default='bert-base-uncased', help="For BERT: 'bert-base-uncased', \
                                                                                                 'bert-large-uncased',\
                                                                                     For ALBERT: 'albert-base-v2',\
                                                                                                 'albert-large-v2',\
                                                                                     For BioBERT: 'bert-base-uncased' (biobert_v1.1_pubmed)")
+    parser.add_argument('--spacy_model_name', type=str, default='en_core_web_lg')
 
     args = parser.parse_args()
 
